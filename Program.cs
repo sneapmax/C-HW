2c2788924a149c83443092dd16da83f51eb30f8a
«Sneap» <sneap@mail.ru>
2022-06-08 22:07:20 +0700

    задача2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+Console.WriteLine("Первое задание");
+
 Console.WriteLine("enter the first number");
 int number1 = int.Parse(Console.ReadLine());
 
@@ -8,13 +10,10 @@ int number2 = int.Parse(Console.ReadLine());
 
 if (number1 > number2)
 {
-    Console.WriteLine("number1 > number2");
+    Console.WriteLine($"{number1} > {number2}");
 }
     else 
 {
-    Console.WriteLine("number2 > number1");
+    Console.WriteLine($"{number2} > {number1}");
 }
-
-
-
 
