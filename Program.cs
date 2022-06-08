1da746f7bc4a6e8c861a520c908c2ffe98a0c76c
«Sneap» <sneap@mail.ru>
2022-06-08 22:18:14 +0700

    задача4

--- Program.cs
+++ Program.cs
@@ -17,3 +17,32 @@ if (number1 > number2)
     Console.WriteLine($"{number2} > {number1}");
 }
 
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Второе задание");
+
+Console.WriteLine("enter the first number");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter the second number");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter the third number");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1
+
+if (max < number2)
+{
+    max = number2;
+}
+    else 
+    if (max < number3)
+{
+     max = number3;
+}
+    else
+{
+     Console.WriteLine($"max number = {max}");
+}    
