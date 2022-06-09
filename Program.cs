f8adeab24e5d7a23211c8be946bf4a02392d9ebe
«Sneap» <sneap@mail.ru>
2022-06-09 14:19:54 +0700

    all  HW

--- Program.cs
+++ Program.cs
@@ -17,32 +17,62 @@ if (number1 > number2)
     Console.WriteLine($"{number2} > {number1}");
 }
 
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 Console.WriteLine("Второе задание");
 
 Console.WriteLine("enter the first number");
-int number1 = int.Parse(Console.ReadLine());
+int numberA = int.Parse(Console.ReadLine());
 
 Console.WriteLine("enter the second number");
-int number2 = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
 
 Console.WriteLine("enter the third number");
-int number3 = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
 
-int max = number1
+int max = numberA;
 
-if (max < number2)
+if (max < numberB)
 {
-    max = number2;
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+
+Console.WriteLine($"max number = {max}");
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Третье задание");
+
+Console.WriteLine("enter the number");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"число {number} четное");
 }
     else 
-    if (max < number3)
 {
-     max = number3;
-}
-    else
-{
-     Console.WriteLine($"max number = {max}");
+    Console.WriteLine($"число {number} нечетное");
 }    
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Четвертое задание");
+
+Console.WriteLine("enter the number");
+int N = int.Parse(Console.ReadLine());
+int countN = 2;
+
+while (countN <= N)
+{
+    Console.Write($"{countN} ");
+    countN += 2;
+}
+
+Console.Write("the end))");
